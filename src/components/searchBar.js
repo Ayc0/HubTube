a62fc76d6ea950c59e3e2e7e4e0bf3b063bf89d0
Ayc0 <benjamin.koltes@student.ecp.fr>
2017-03-13 16:03:22 +0100

    jolie searchbar

--- src/components/searchBar.js
+++ src/components/searchBar.js
@@ -1,47 +1,14 @@
 import React, { Component } from 'react';
 import AutoComplete from 'material-ui/AutoComplete';
 import Search from 'material-ui/svg-icons/action/search';
-
-import './searchBar.css';
-
-const iconStyle = {
-  padding: '.6rem 1rem',
-  marginBottom: 0,
-  fontSize: '1rem',
-  fontWeight: 400,
-  lineHeight: 1.25,
-  color: '#464a4c',
-  textAlign: 'center',
-  border: '1px solid rgba(0,0,0,.15)',
-  borderRadius: '.25rem',
-  borderLeft: 0,
-  borderBottomLeftRadius: 0,
-  borderTopLeftRadius: 0,
-};
-
-const inputStyle = {
-  height: '20px',
-  padding: '.75rem .75rem',
-  fontSize: '1rem',
-  lineHeight: '1.25',
-  color: '#464a4c',
-  backgroundColor: '#fff',
-  backgroundImage: 'none',
-  backgroundClip: 'padding-box',
-  border: '1px solid rgba(0, 0, 0, 0.15)',
-  borderRadius: '.25rem',
-  flex: '1 1 auto',
-  width: '1%',
-  marginBottom: 0,
-  borderBottomRightRadius: 0,
-  borderTopRightRadius: 0,
-};
+import { Toolbar, ToolbarGroup } from 'material-ui/Toolbar';
+import IconButton from 'material-ui/IconButton';
 
 export default class SearchBar extends Component {
   constructor(props) {
     super(props);
 
-    this.state = { query: '' };
+    this.state = { query: '', value: 3 };
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
     this.onTextChange = this.onTextChange.bind(this);
@@ -72,19 +39,26 @@ export default class SearchBar extends Component {
       dataSource = this.props.dataSource;
     }
     return (
-      <form style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}
-        onSubmit={this.handleSubmit}>
-        <AutoComplete hintText="Search" underlineShow={false} style={inputStyle}
-          onUpdateInput={(newValue) => {
-            this.handleChange(newValue);
-            this.onTextChange(newValue);
-          }}
-          dataSource={dataSource}
-          value={this.state.query}
-          fullWidth={true}
-        />
-        <Search className="validate_search" style={iconStyle}
-          onClick={this.handleSubmit} />
+      <form onSubmit={this.handleSubmit}>
+        <Toolbar>
+          <ToolbarGroup style={{ width: '98%' }}>
+            <AutoComplete hintText="Search" underlineShow={false}
+              onUpdateInput={(newValue) => {
+                this.handleChange(newValue);
+                this.onTextChange(newValue);
+              }}
+              dataSource={dataSource}
+              value={this.state.query}
+              fullWidth={true}
+            />
+          </ToolbarGroup>
+          <ToolbarGroup>
+            <IconButton>
+              <Search className="validate_search"
+                onClick={this.handleSubmit} />
+            </IconButton>
+          </ToolbarGroup>
+        </Toolbar>
       </form>
     );
   }
